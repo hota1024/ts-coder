79968d6543affac7ddb5ad062a7006b6cb7e7643
hota1024 <hotalog@hota1024.com>
2020-11-16 12:35:57 +0900

    :tada: Init

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import * as path from 'path'
+import sourcemaps from 'rollup-plugin-sourcemaps'
+import typescript from 'rollup-plugin-typescript2'
+
+/** @type {import('rollup').RollupOptions} */
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: path.resolve(__dirname, 'dist/index.js'),
+      name: 'TsCoder',
+      format: 'umd',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    typescript({
+      typescript: require('ttypescript'),
+      tsconfigDefaults: {
+        compilerOptions: {
+          plugins: [{ transform: '@zerollup/ts-transform-paths' }],
+        },
+      },
+      useTsconfigDeclarationDir: true,
+    }),
+    sourcemaps(),
+  ],
+}
